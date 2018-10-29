510699f73990477c0f51f2fcfa6023da757f289e
kmalillos <malillosk@gmail.com>
2018-10-28 20:53:23 -0600

    added initial files and starter code

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyCk9iinDB6nzk-7yvGYupOiB5e-EZ_U8kg",
+authDomain: "traintimes-55235.firebaseapp.com",
+databaseURL: "https://traintimes-55235.firebaseio.com",
+projectId: "traintimes-55235",
+storageBucket: "traintimes-55235.appspot.com",
+messagingSenderId: "369485631475"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
